9fa18c05b7f50055f8152c589ca90a36db66584a
iBlockchainer <blockchainer.me@gmail.com>
2022-03-04 13:16:31 +0500

    new changes

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-export const fbaseUrl = "http://localhost:3000";
-// export const fbaseUrl = "https://lmss.netlify.app";
+// export const fbaseUrl = "http://localhost:3000";
+export const fbaseUrl = "https://dcuchat.netlify.app";
 // export const baseUrl = "http://localhost:5000";
 export const baseUrl = "https://dcuchat.herokuapp.com";
 export default axios.create({
